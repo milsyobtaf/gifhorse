817ac674ea6d125cab7dacfff8f03783d8b2d925
Flip Stewart <flipstewart@me.com>
2015-08-04 01:25:06 -0500

    do the palette thing and incidentally rewrite most everything fixes #6

--- gifhorse.js
+++ gifhorse.js
@@ -1,11 +1,10 @@
 #! /usr/bin/env node
 
 var fs = require('fs');
+var exec = require('child_process').exec;
 var crypto = require('crypto');
 var packageJson = require('./package.json');
 var program = require('commander');
-var Ffmpeg = require('ffmpeg');
-var imagemagick = require('imagemagick');
 var chalk = require('chalk');
 
 var cmdInput, cmdOutput;
@@ -42,57 +41,55 @@ if (typeof cmdInput !== 'string' || typeof cmdOutput !== 'string') {
   process.exit(1);
 } else {
   program.input = cmdInput;
-  program.output = cmdOutput.replace(/\.[^.]*$/, '');
-  program.filename = cmdOutput.match(/[^\/]+(?=\.)/);
+  program.output = cmdOutput;
 }
 
-function annotate(file) {
+function annotate() {
   logger.good('words are being put on the thing!');
-  var pointsize = program.pointsize || 15;
-  var options = [
-    file,
-    '-pointsize', pointsize,
+  exec([
+    'convert', program.annotationPath,
+    '-pointsize', program.pointsize || (program.pointsize = 15),
+    program.fontface ? '-font' : '', program.fontface ? program.fontface : '',
     '-strokewidth', '2',
     '-stroke', 'black',
     '-fill', 'white',
     '-gravity', 'south',
-    '-annotate', '+0+' + pointsize / 2, program.annotation,
+    '-annotate', '+0+' + program.pointsize / 2, '"' + program.annotation + '"',
     '-stroke', 'none',
-    '-annotate', '+0+' + pointsize / 2, program.annotation,
-    program.output + '.gif'
-  ];
-  program.fontface && options.splice(3, 0, '-font', program.fontface);
-  return imagemagick.convert(options, function(error) {
-    fs.unlink(file);
-    !error
-      ? logger.good(program.output + '.gif exists now probably!')
-      : logger.bad(error);
-  });
+    '-annotate', '+0+' + program.pointsize / 2, '"' + program.annotation + '"',
+    program.output
+  ].join(' '));
 }
 
 try {
-  var ffmpegProcess = new Ffmpeg(program.input);
-  return ffmpegProcess.then(function (video) {
-    logger.good('video processing is happening!');
-    var path = program.annotation
-      ? '/tmp/' + (crypto.randomBytes(20).toString('hex')) + '.m4v'
-      : program.filename + '.gif';
-    video.addCommand('-ss', program.start || '00:00:00');
-    video.addCommand('-t', program.duration || '99:99:99');
-    video
-      .setDisableAudio()
-      .setVideoSize(program.width ? program.width + 'x?' : '320x?', true, false)
-      .setVideoFrameRate(program.fps || 15)
-      .save(path, function (error, file) {
-        return !error
-          ? program.annotation
-            ? annotate(file)
-            : logger.good(file + ' exists now probably!')
-          : logger.bad(error);
-      });
-  }, function (error) {
-    return logger.bad(error);
-  });
+  program.palette = '/tmp/palette.png';
+  program.filters = 'fps=' + program.fps + ',scale=' + (program.width || 320) + ':-1:flags=lanczos';
+  program.annotationPath = program.annotation && '/tmp/' + crypto.randomBytes(20).toString('hex') + '.m4v';
+
+  logger.good('a palette is being did!');
+  exec([
+    'ffmpeg',
+    '-ss', program.start || '00:00:00',
+    '-t', program.duration || '99:99:99',
+    '-i', program.input,
+    '-vf', program.filters + ',palettegen',
+    '-y', '/tmp/palette.png'
+  ].join(' '));
+
+  logger.good('video processing is happening!');
+  exec([
+    'ffmpeg',
+    '-ss', program.start || '00:00:00',
+    '-t', program.duration || '99:99:99',
+    '-i', program.input,
+    '-i', program.palette,
+    '-lavfi', '"' + program.filters + ' [x]; [x][1:v] paletteuse' + '"',
+    '-y', program.annotationPath ? program.annotationPath : program.output
+  ].join(' '), program.annotation
+    ? annotate
+    : logger.good(path + ' exists now probably!')
+  );
+
 } catch (exception) {
   return exception.code
     ? logger.bad(exception.code + ' ' + exception.msg)
